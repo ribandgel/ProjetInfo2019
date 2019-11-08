d083e28202528879c66b977250b74d57286eea5f
carole <karole.m@live.frgit config --global user.email karole.m@live.frgit config --global user.email karole.m@live.fr>
2019-11-08 09:09:39 -0800

    solved issue with inheritance of beverage

--- src/Entity/Beverage.php
+++ src/Entity/Beverage.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\InheritanceType;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BeverageRepository")
+ * @InheritanceType("JOINED")
  */
 class Beverage
 {
