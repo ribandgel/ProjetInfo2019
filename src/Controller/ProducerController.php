fdb5c907c018ef97abe03f4b6290f0eec3a867b2
Andgel <ribeiroandgel@gmx.fr>
2019-11-21 11:18:34 +0100

    latitude and longitude calcul if are set to null

---
+++ src/Controller/ProducerController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Producer;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use Symfony\Component\HttpClient\HttpClient;
+
+class ProducerController extends EasyAdminController
+{
+        protected function updateEntity($entity)
+        {
+            if($entity instanceof Producer && ($entity->getLatitude() == null || $entity->getLongitude() == null) ){
+                $address = $entity->getAddress();
+                $parsedAddressLines = str_replace(' ','-',$address->getAddressLines());
+                $parsedCity = str_replace(' ','-',$address->getCity());
+                $client = HttpClient::create();
+                $response = $client->request('GET', 'http://www.mapquestapi.com/geocoding/v1/address?key=y0TITKwvxumckEPFoEJrVoPCccjlBwyl&location='
+                    .$address->getCountry()
+                    .$address->getZipCode()
+                    .$parsedCity
+                    .$parsedAddressLines);
+                $array = json_decode($response->getContent(),true);
+                if(isset($array['results'][0]['locations'][0]['latLng']['lat'])){
+                    $entity->setLatitude($array['results'][0]['locations'][0]['latLng']['lat']);
+                }
+                if(isset($array['results'][0]['locations'][0]['latLng']['lng'])){
+                    $entity->setLongitude($array['results'][0]['locations'][0]['latLng']['lng']);
+                }
+            }
+            parent::updateEntity($entity);
+        }
+}
