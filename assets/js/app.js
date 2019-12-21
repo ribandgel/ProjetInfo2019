36b8d4bfd626aa8c1056f601f0b2d760c210f201
carole <karole.m@live.frgit config --global user.email karole.m@live.frgit config --global user.email karole.m@live.fr>
2019-12-21 12:19:10 -0800

    changed map page: I added different maps to be displayed (wineries map, designations map, varieties map) and added a menu to choose map. I styled the search filter better and added a button to hide search

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,6 @@ if(title == "Accueil") {
  require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+//const $ = require('jquery');
 //
 // console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
